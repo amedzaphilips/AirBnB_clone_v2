fb49491f05ccaa58a13091d7850fd4162f46d045
amedzaphilips <danielamedzaphilips@gmail.com>
2024-08-03 12:10:08 -0700

    2-c Variable added to function

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,9 +19,9 @@ def hbnb():
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c():
+def c(text):
     text = text.replace('_', ' ')
     return f"C {text}"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)

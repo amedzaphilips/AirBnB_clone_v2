513ffa749f6aea6e744ef17f8650e4c24af701c2
amedzaphilips <danielamedzaphilips@gmail.com>
2024-08-03 14:25:18 -0700

    5 changes

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,9 +38,9 @@ def number(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def template(n):
-    if isinstance(n, int):
-        return render_template('5-number.html', n=n)
+def template_num(n):
+    '''if isinstance(n, int):'''
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':

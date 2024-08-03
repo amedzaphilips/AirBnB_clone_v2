54617a729ea3a8a09c0f0b807b8933d0c169dc20
amedzaphilips <danielamedzaphilips@gmail.com>
2024-08-03 12:32:51 -0700

    $ is a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+'''
+    a script that starts a Flask web application
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    if isinstance(n, int):
+        return "n is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
